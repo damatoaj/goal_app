d6b9eb4fe15084cd6c36e5652baeb46d5ca9722a
damatoaj <mr.arthurdamato@gmail.com>
2022-01-05 10:24:43 -0800

    update process goals route complete

--- backend/controllers/process.js
+++ backend/controllers/process.js
@@ -21,9 +21,25 @@ const edit = (req, res) => {
     res.send('edit process')
 };
 
-const update = (req, res) => {
-    console.log('update process')
-    res.send('update process')
+const update = async (req, res) => {
+    const updates = Object.keys(req.body);
+    try {
+        const object = await Outcome.findOne({"performanceGoals.processGoals._id":req.params.id})
+        if (!object) return res.status(404).send();
+        const performanceGoal = object.performanceGoals.filter(performance => {
+            return performance.processGoals.find(process => {
+                return process._id.toString() === req.params.id;
+            })
+        });
+        performanceGoal[0].processGoals.forEach(goal => {
+            if(goal._id.toString() === req.params.id) {
+                updates.forEach((update)=> goal[update] = req.body[update])
+            }
+        });
+        res.send(object)
+    } catch (e) {
+        res.status(500).send();
+    }
 };
 
 const deletePro = (req, res) => {
